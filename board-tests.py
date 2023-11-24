f6928e6b0a32144fb27446a1c8b27b6d8f563f73
rwm275 <rwm275@cornell.edu>
2023-11-23 22:19:58 -0500

    board printing

--- board-tests.py
+++ board-tests.py
@@ -3,5 +3,12 @@ import board
 def test_conversions():
   for ch in [chr(i) for i in range(97, 105)]:
     for j in range(1, 9):
-      print(ch, j)
+      # print(ch, j)
       assert(board.int_to_square(board.square_to_int(ch, j)) == (ch, j))
+
+
+if __name__ == "__main__":
+  print("Tests:")
+  # test_conversions()
+  game = board.Game()
+  game.pretty_print()
--- board.py
+++ board.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List
 
 '''
 square: chess board label for a cell (e.g. 'c3')
@@ -11,8 +11,7 @@ def square_to_int(row: str, col: int) -> Optional[int]:
   Convert a row, col pair into integer representation
   '''
   if len(row) == 1 and row >= 'a' and row <= 'h' and col >= 1 and col <= 8:
-    c = 1 << ((col - 1) * 8 + (ord(row) - ord('a')))
-    return np.uint64(c)
+    return np.uint64(1 << ((col - 1) * 8 + (ord(row) - ord('a'))))
 
 def int_to_square(num: int) -> Optional[Tuple[str, int]]:
   '''
@@ -29,5 +28,91 @@ def int_to_square(num: int) -> Optional[Tuple[str, int]]:
     return row, col
 
 class Player:
-  def __init__(self, color):
-    pass
+  pawn, rook, knight, bishop, king, queen = 'p', 'r', 'h', 'b', 'k', 'q'
+  w_icons = {
+     'p': '♙', 
+     'r': '♖', 
+     'h': '♘', 
+     'b': '♗', 
+     'k': '♔', 
+     'q': '♕'
+  }
+  b_icons = {
+     'p': '♟', 
+     'r': '♜', 
+     'h': '♞', 
+     'b': '♝', 
+     'k': '♚', 
+     'q': '♛'
+  }
+
+  def __init__(self, white: bool):
+    self.pawn = np.uint64(0)
+    self.rook = np.uint64(0)
+    self.knight = np.uint64(0)
+    self.bishop = np.uint64(0)
+    self.king = np.uint64(0)
+    self.queen = np.uint64(0)
+    self.reset(white)
+
+  def reset(self, white: bool) -> None:
+    self.pawn = np.uint64(0)
+    if white:
+      for i in range(8):
+        self.pawn = self.pawn | np.uint64(1 << (8 + i))
+      self.rook = np.uint64(1) | np.uint64(1 << 7)
+      self.knight = np.uint64(2) | np.uint64(1 << 6)
+      self.bishop = np.uint64(4) | np.uint64(1 << 5)
+      self.queen = np.uint64(8)
+      self.king = np.uint64(16)
+    else:
+      for i in range(8):
+        self.pawn = self.pawn | np.uint64(1 << (48 + i))
+      self.rook = np.uint64(1 << 56) | np.uint64(1 << 63)
+      self.knight = np.uint64(1 << 57) | np.uint64(1 << 62)
+      self.bishop = np.uint64(1 << 58) | np.uint(1 << 61)
+      self.queen = np.uint(1 << 59)
+      self.king = np.uint(1 << 60)
+
+  def piece_at_int(self, num: int) -> Optional[str]:
+    num = np.uint64(num)
+    if self.pawn & num: return Player.pawn
+    if self.rook & num: return Player.rook
+    if self.knight & num: return Player.knight
+    if self.bishop & num: return Player.bishop
+    if self.queen & num: return Player.queen
+    if self.king & num: return Player.king
+  
+
+
+class Game:
+    def __init__(self):
+        self.player1 = Player(white=True)
+        self.player2 = Player(white=False)
+
+    def pretty_print(self):
+        board = [['.' for _ in range(8)] for _ in range(8)]
+        for i in range(8):
+            for j in range(8):
+                piece1 = self.player1.piece_at_int(square_to_int(chr(ord('a') + i), j + 1))
+                piece2 = self.player2.piece_at_int(square_to_int(chr(ord('a') + i), j + 1))
+                if piece1:
+                    board[j][i] = 'w' + piece1
+                elif piece2:
+                    board[j][i] = 'b' + piece2
+
+        print("    a   b   c   d   e   f   g   h")
+        print("    -----------------------------")
+        for i in range(8):
+            print(f" {8 - i} ", end="")
+            for j in range(8):
+                piece = board[i][j]
+                if piece[0] == 'w':
+                    print(f" {Player.w_icons[piece[1]]} ", end=" ")  # White piece
+                elif piece[0] == 'b':
+                    print(f" {Player.b_icons[piece[1]]} ", end=" ")  # Black piece
+                else:
+                    print(" • ", end=" ")  # Empty square
+            print(f" {8 - i}")
+        print("    -----------------------------")
+        print("    a   b   c   d   e   f   g   h")
